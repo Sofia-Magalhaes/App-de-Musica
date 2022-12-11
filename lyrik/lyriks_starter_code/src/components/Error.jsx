cf4e7f450b214ff96f0093040f80a7b38c4aae11
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-11 19:24:42 -0300

    API + Loader + Error
    Mexendo com Rapid API Music Fetching, Loader Component e Error Component

--- lyrik/lyriks_starter_code/src/components/Error.jsx
+++ lyrik/lyriks_starter_code/src/components/Error.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable */
+
 import React from 'react';
 
 const Error = () => (
-  <div>Error</div>
+  <div className='w-full flex justify-center items-center'>
+    <h1 className='font-bold text-2xl text-white mt-2'>Alguma coisa está errada, por favor tente novamente</h1>
+  </div>
 );
 
 export default Error;
---
+++ lyrik/lyriks_starter_code/src/redux/services/shazamCore.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'a9d8ecc92amshe35636177eb9f51p154d26jsnde67b92106d6');
+
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => '/charts/world' }),
+    }),
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
