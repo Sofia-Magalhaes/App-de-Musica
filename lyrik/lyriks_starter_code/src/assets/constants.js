1f9741606a81eb5e540cf66e5b8c6d889e686fc7
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-11 22:34:13 -0300

    seção - top play

--- lyrik/lyriks_starter_code/src/assets/constants.js
+++ lyrik/lyriks_starter_code/src/assets/constants.js
@@ -4,12 +4,12 @@ export const genres = [
   { title: 'Pop', value: 'POP' },
   { title: 'Hip-Hop', value: 'HIP_HOP_RAP' },
   { title: 'Dance', value: 'DANCE' },
-  { title: 'Electronic', value: 'ELECTRONIC' },
+  { title: 'Electronica', value: 'ELECTRONIC' },
   { title: 'Soul', value: 'SOUL_RNB' },
-  { title: 'Alternative', value: 'ALTERNATIVE' },
+  { title: 'Alternativo', value: 'ALTERNATIVE' },
   { title: 'Rock', value: 'ROCK' },
-  { title: 'Latin', value: 'LATIN' },
-  { title: 'Film', value: 'FILM_TV' },
+  { title: 'Latino', value: 'LATIN' },
+  { title: 'Filme', value: 'FILM_TV' },
   { title: 'Country', value: 'COUNTRY' },
   { title: 'Worldwide', value: 'WORLDWIDE' },
   { title: 'Reggae', value: 'REGGAE_DANCE_HALL' },
@@ -18,8 +18,8 @@ export const genres = [
 ];
 
 export const links = [
-  { name: 'Discover', to: '/', icon: HiOutlineHome },
-  { name: 'Around You', to: '/around-you', icon: HiOutlinePhotograph },
-  { name: 'Top Artists', to: '/top-artists', icon: HiOutlineUserGroup },
+  { name: 'Descobrir', to: '/', icon: HiOutlineHome },
+  { name: 'Perto de Você', to: '/around-you', icon: HiOutlinePhotograph },
+  { name: 'Top Artistas', to: '/top-artists', icon: HiOutlineUserGroup },
   { name: 'Top Charts', to: '/top-charts', icon: HiOutlineHashtag },
 ];
