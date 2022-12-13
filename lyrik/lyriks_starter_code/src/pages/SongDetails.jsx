cf6f47c5b4bea5754987d03958a38d744ce14f5b
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-12 21:35:18 -0300

    song details page
    mostra os dados da música, letra e related songs

--- lyrik/lyriks_starter_code/src/pages/SongDetails.jsx
+++ lyrik/lyriks_starter_code/src/pages/SongDetails.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable */
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
-import { useParams } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
+import { useGetSongDetailsQuery, useGetSongRelatedQuery } from '../redux/services/shazamCore';
 
-import { setActiveSong, playPause } from "../redux/features/playerSlice";
-import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
 
 
 const SongDetails = () => {
@@ -13,26 +13,43 @@ const SongDetails = () => {
     const { songid } = useParams();
     const { activeSong, isPlaying } = useSelector((state) => state.player);
     const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
+    const { data, isFetching: isFetchingRelatedSongs, error } = useGetSongRelatedQuery({ songid });
 
-    console.log(songid);
+    const handlePauseClick = () => {
+        dispatch(playPause(false));
+    };
+
+    const handlePlayClick = (song, i) => {
+        dispatch(setActiveSong({ song, data, i }));
+        dispatch(playPause(true));
+    };
+
+    if (isFetchingSongDetails || isFetchingRelatedSongs) return <Loader title="Procurando detalhes da música" />;
+
+    if (error) return <Error />;
+
 
     return (
-        <div className="flex flex-col">
-            {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
+        <div className='flex flex-col'>
+            <DetailsHeader artistId="" songData={songData} />
 
-            <div className="mb-10">
-                <h2 className="text-white text-3xl font-bold">Letras: </h2>
-
-                <div className="mt-5">
-                    {
-                        songData?.sections[1].typee === 'LETRAS' ? songData?.sections[1].text.map((line, i) => (
-                            <p className="text-gray-400 text-base my-1">{line}</p>
-                        )) :  <p className="text-gray-400 text-base my-1">Desculpa, lentra não encontrada</p>
-                    }                                        
+            <div className='mb-10'>
+                <h2 className='text-white text-3xl font-bold'>LETRA: </h2>
+                <div className='mt-5'>
+                    {songData?.sections[1].type === 'LYRICS' ? songData?.sections[1].text.map((line, i) => (
+                        <p className='text-gray-400 text-base my-1'>{line}</p>
+                    )) : <p className='text-gray-400 text-base my-1'>Desculpa, não encontramos a letra para essa música!</p>}
                 </div>
             </div>
+            <RelatedSongs
+                data={data}
+                isPlaying={isPlaying}
+                activeSong={activeSong}
+                handlePauseClick={handlePauseClick}
+                handlePlayClick={handlePlayClick}
+            />
         </div>
     );
+};
 
-}
 export default SongDetails;
